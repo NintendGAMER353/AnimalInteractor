31ed115b3773a18ec8650d6373c2b49d7aabc017
meskion <manufdh@hotmail.com>
2026-03-08 20:24:25 +0100

    Pause Menu

--- Assets/_MyAssets/Scripts/GameManager.cs
+++ Assets/_MyAssets/Scripts/GameManager.cs
@@ -7,11 +7,28 @@ public class GameManager : MonoBehaviour
 
     public static GameManager Instance;
 
+    public GameObject pausePanel;
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
         }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void OnContinue()
+    {
+        Time.timeScale = 1.0f;
+        pausePanel.SetActive(false);
     }
 }
